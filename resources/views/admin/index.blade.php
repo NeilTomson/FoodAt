64b02037edecfd34a1988e5c987a972b976f7f61
NeilTomson <attilatoaso@yahoo.com>
2022-12-20 16:14:14 +0200

    finis

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
+<div class="container-fluid d-flex flex-row  w-full justify-content-around main">
 
-<h1>admin</h1>
+    <div class="graficons d-flex flex-column  w-full justify-content-center align-items-center">
+        <div>
+            <h1>Wellcome back in Admin page</h1>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +17,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/users/home', [HomeController::class, 'index'])->name('home');
+Route::get('/redirects', [HomeController::class, 'redirects'])->name('home');
+
+// user
+Route::post('/addcard', [HomeController::class, 'addcard']);
+Route::post('/addcard/{id}', [HomeController::class, 'addcard']);
+Route::post('/orderConfirm', [HomeController::class, 'orderconfirm']);
+Route::get('/showCard/{id}', [HomeController::class, 'showCard']);
+Route::get('/removeOrder/{id}', [HomeController::class, 'removeOrder']);
+
+// admin user
+Route::get('/users', [AdminController::class, 'user']);
+Route::get('/deleteuser/{id}', [AdminController::class, 'deleteuser']);
+
+
+// amind cheff
+Route::get('/cheff', [AdminController::class, 'cheff']);
+Route::get('/editchef/{id}', [AdminController::class, 'editchef']);
+Route::get('/deletechef/{id}', [AdminController::class, 'deletechef']);
+Route::post('/updatechef/{id}', [AdminController::class, 'updateChef']);
+Route::post('/uploadchef', [AdminController::class, 'uploadchef']);
+
+
+// admin menu
+Route::get('/foodmenu', [AdminController::class, 'foodmenu']);
+Route::get('/editmenu/{id}', [AdminController::class, 'editmenu']);
+Route::get('/deletemenu/{id}', [AdminController::class, 'deletemenu']);
+Route::post('/updatmenu/{id}', [AdminController::class, 'updatemenu']);
+Route::post('/upload', [AdminController::class, 'uploadmenu']);
+
+// admin reservation 
+Route::post('/reservation', [AdminController::class, 'reservation']);
+Route::get('/deletereservation/{id}', [AdminController::class, 'deletereservation']);
+
+Route::get('/Adminreservation', [AdminController::class, 'showreservation']);
+Route::get('/search', [AdminController::class, 'search']);
+Route::get('/Adminreservation', [AdminController::class, 'showreservation']);
+
+// admin order
+Route::get('/orders', [AdminController::class, 'orderShow']);
+Route::get('/deleteorders/{id}', [AdminController::class, 'deleteorders']);
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/redirects', [App\Http\Controllers\HomeController::class, 'redirects'])->name('home');
